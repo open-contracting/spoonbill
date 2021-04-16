518f45e412bde67a0cefcfb6f4d139b13fe1bb41
yshalenyk <yshalenyk@gmail.com>
2021-04-16 15:23:58 +0300

    fix:pretty_headers and override headers are now used in writers

--- spoonbill/writers/csv.py
+++ spoonbill/writers/csv.py
@@ -1,7 +1,24 @@
 import csv
+import logging
+
+from spoonbill.i18n import _
+from spoonbill.utils import get_headers
+
+
+LOGGER = logging.getLogger('spoonbill')
 
 
 class CSVWriter:
+    """Writer class with output to csv files
+
+    For each table will be created corresponding csv file
+
+    :param workdir: Working directory
+    :param tables: Tables data
+    :options: Flattening options
+    """
+
+    name = "csv"
 
     def __init__(self, workdir, tables, options):
         self.workdir = workdir
@@ -9,24 +26,28 @@ class CSVWriter:
         self.fds = []
         self.tables = tables
         self.options = options
-        for name, table in tables.items():
-            # if table.total_rows == 0:
-            # LOGGER.info("Skipping table %s as empty")
-            # continue
-            split = options.selection[name].split
+        self.headers = {}
+        for name, table in self.tables.items():
+            opt = self.options.selection[name]
+            headers = get_headers(table, opt)
+            self.headers[name] = headers
             fd = open(workdir / f"{name}.csv", "w")
-            writer = csv.DictWriter(fd, table.available_rows(split=split))
+            writer = csv.DictWriter(fd, headers)
             self.fds.append(fd)
             self.writers[name] = writer
 
     def writeheaders(self):
         """Write headers to output file"""
-        for w in self.writers.values():
-            w.writeheader()
+        for name, writer in self.writers.items():
+            headers = self.headers[name]
+            writer.writerow(headers)
 
     def writerow(self, table, row):
         """Write row to output file"""
-        self.writers[table].writerow(row)
+        try:
+            self.writers[table].writerow(row)
+        except ValueError as err:
+            LOGGER.error(_("Failed to write row {} with error {}").format(row['rowID'], err))
 
     def close(self):
         """Finish work"""
--- spoonbill/writers/xlsx.py
+++ spoonbill/writers/xlsx.py
@@ -1,9 +1,27 @@
-import collections
+from xlsxwriter.exceptions import XlsxWriterException
 
 import xlsxwriter
+import collections
+import logging
+
+from spoonbill.i18n import _
+from spoonbill.utils import get_headers
+
+
+LOGGER = logging.getLogger('spoonbill')
 
 
 class XlsxWriter:
+    """Writer class with output to xlsx files
+
+    For each table will be created corresponding sheet inside workbook
+
+    :param workdir: Working directory
+    :param tables: Tables data
+    :options: Flattening options
+    """
+    name = "xlsx"
+
     def __init__(self, workdir, tables, options):
         self.workdir = workdir
         self.workbook = xlsxwriter.Workbook(
@@ -17,14 +35,18 @@ class XlsxWriter:
     def writeheaders(self):
         """Write headers to output file"""
         for name, table in self.tables.items():
-            # if spec[name]['total_rows'] == 0:
-            # continue
-            split = self.options.selection[name].split
+            opt = self.options.selection[name]
+            split = opt.split
             sheet = self.workbook.add_worksheet(name)
-            headers = table.available_rows(split=split)
+            headers = get_headers(table, opt)
             for col_index, col_name in enumerate(headers):
                 self.col_index[name][col_name] = col_index
-                sheet.write(0, col_index, col_name)
+                try:
+                    sheet.write(0, col_index, headers[col_name])
+                except XlsxWriterException as err:
+                    LOGGER.error(_("Failed to write header {} to xlsx sheet {} with error {}").format(
+                        col_name, name, err
+                    ))
             self.row_counters[name] = 1
 
     def writerow(self, table, row):
@@ -33,7 +55,13 @@ class XlsxWriter:
 
         for column, value in row.items():
             col_index = self.col_index[table][column]
-            sheet.write(self.row_counters[table], col_index, value)
+            try:
+                sheet.write(self.row_counters[table], col_index, value)
+            except XlsxWriterException as err:
+                LOGGER.error(_("Failed to write column {} to xlsx sheet {} with error {}").format(
+                    column, table, err
+                ))
+
         self.row_counters[table] += 1
 
     def close(self):
