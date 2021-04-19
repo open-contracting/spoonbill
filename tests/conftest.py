f2d14e0f457dca87a321babfa14ad9d01fbfdf44
yshalenyk <yshalenyk@gmail.com>
2021-04-19 17:21:54 +0300

    test: add spec_analyzed fixture to use with DataPreprocessor.restore

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from .data import TEST_ROOT_TABLES, TEST_COMBINED_TABLES
 here = pathlib.Path(__file__).parent
 schema_path = here / "data" / "ocds-simplified-schema.json"
 releases_path = here / "data" / "ocds-sample-data.json"
-
+analyzed_path = here / "data" / "analyzed.json"
 
 @pytest.fixture
 def schema():
@@ -36,3 +36,13 @@ def spec(schema):
     return DataPreprocessor(
         schema, TEST_ROOT_TABLES, combined_tables=TEST_COMBINED_TABLES
     )
+
+
+@pytest.fixture
+def spec_analyzed():
+    with open(analyzed_path) as fd:
+        data =  json.load(fd)
+    dp = DataPreprocessor.restore(data)
+    return dp
+
+
