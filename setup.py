fd330ae7e455a2a15a8bc9cc76af6ed32f5d3a3c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-05-27 17:48:18 -0400

    setup: Put requirements inline. Use MANIFEST.in instead of package_data and include_package_data.

--- setup.py
+++ setup.py
@@ -3,44 +3,43 @@ from setuptools import find_packages, setup
 with open("README.rst") as f:
     long_description = f.read()
 
-
-requires = [
-    "ijson>=2.5",
-    "jsonref",
-    "jsonpointer",
-    "xlsxwriter",
-    "requests",
-    "click",
-    "click_logging",
-    "ocdskit",
-    "ocdsextensionregistry",
-    'dataclasses;python_version<"3.7"',
-]
-test_requires = ["pytest", "jmespath", "pytest-cov", "coveralls", "openpyxl", "jsonpointer"] + requires
-docs_requires = [
-    "Sphinx",
-    "sphinx-autobuild",
-    "sphinx-rtd-theme",
-    "sphinx_click",
-]
-
 setup(
     name="spoonbill",
     version="1.0.1b1",
     author="Open Contracting Partnership",
     author_email="data@open-contracting.org",
     url="https://github.com/open-contracting/spoonbill",
-    description="An instrument to flatten OCDS data",
+    description="Converts data that conforms to the Open Contracting Data Standard from JSON to Excel / CSV",
     license="BSD",
     packages=find_packages(exclude=["tests", "tests.*"]),
     long_description=long_description,
-    install_requires=requires,
+    install_requires=[
+        "click",
+        "click_logging",
+        "ijson>=2.5",
+        "jsonpointer",
+        "jsonref",
+        "ocdsextensionregistry",
+        "ocdskit",
+        "requests",
+        "xlsxwriter",
+        'dataclasses;python_version<"3.7"',
+    ],
     extras_require={
-        "test": test_requires,
-        "docs": docs_requires,
+        "test": [
+            "coveralls",
+            "jmespath",
+            "openpyxl",
+            "pytest",
+            "pytest-cov",
+        ],
+        "docs": [
+            "sphinx",
+            "sphinx-autobuild",
+            "sphinx-rtd-theme",
+            "sphinx_click",
+        ],
     },
-    package_data={"spoonbill": ["locales/*/*/*.mo", "locales/*/*/*.po"]},
-    include_package_data=True,
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
@@ -50,7 +49,9 @@ setup(
         "Programming Language :: Python :: 3.9",
     ],
     entry_points={
-        "console_scripts": ["spoonbill = spoonbill.cli:cli"],
+        "console_scripts": [
+            "spoonbill = spoonbill.cli:cli",
+        ],
         "babel.extractors": [
             "sp_schema = spoonbill.i18n:extract_schema_po",
         ],
